176b466d8299bd4fad9eaea952b9a0ae0f3ff7a8
fturker80 <fthturker80@gmail.com>
2022-04-14 14:57:29 +0300

    14.04.2022

--- src/_00_denemeler/Q00_denemeler.java
+++ src/_00_denemeler/Q00_denemeler.java
@@ -2,13 +2,7 @@ package _00_denemeler;
 
 public class Q00_denemeler {
     public static void main(String[] args) {
- /* TASK :
-  Elinizde bir miktar farklı boylarda somun ve bu somunlara uyacak civatalar olsun.
-  Civataları somunlara eşlemek için nasıl bir algoritma yazarsınız.
 
-   */
-
-        System.out.println();
     }
 }
 
